3d998acdf5b3ca3c5c69adfeba0a937709aed8d1
ThourayaBchir <tbchir@gmail.com>
2020-11-14 01:47:42 +0100

    add try statement

--- app/routes.py
+++ app/routes.py
@@ -23,7 +23,10 @@ def movies():
 
 
 def get_from_api(url, field: str):
-    field_list = requests.get(url + field).json()
+    try:
+        field_list = requests.get(url + field).json()
+    except requests.exceptions.RequestException:
+        return []
     return field_list
 
 
@@ -31,10 +34,9 @@ def merge_films_and_peoples(films_list, peoples_list):
     df_peoples = pd.DataFrame(peoples_list, columns=['name', 'films'])
     df_peoples = pd.DataFrame({'names': np.repeat(df_peoples.name.values, df_peoples.films.str.len()),
                                'films': np.concatenate(df_peoples.films.values)})
-    df_peoples['film_id'] = df_peoples['films'].apply(lambda x: x.split('/')[-1])
 
-    df_films = pd.DataFrame(films_list, columns=['id', 'title'])
-    df = pd.merge(df_films, df_peoples, left_on='id', right_on='film_id', how='outer').fillna('').groupby(['title'])\
+    df_films = pd.DataFrame(films_list, columns=['url', 'title'])
+    df = pd.merge(df_films, df_peoples, left_on='url', right_on='films', how='outer').fillna('').groupby(['title'])\
         .agg({'names': lambda x: list(x)}).reset_index()
 
     return df.to_dict('records')
